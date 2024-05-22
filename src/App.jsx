053bf2ca1721c55e0d1703d07d16938a6a0e6ff0
Farrelll <137156152+Farrelx9@users.noreply.github.com>
2024-05-22 23:23:51 +0700

    upd

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import "./App.css";
 import Home from "./pages/Home";
+import Login from "./assets/Auth/Login";
 
 export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   );
---
+++ src/assets/Auth/Login.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Login() {
+  return (
+    <div className="bg-gradient-to-r from-green-200 to-white h-screen flex items-center justify-center">
+      <div>
+        <div>Login</div>
+      </div>
+    </div>
+  );
+}
