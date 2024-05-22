db7e7172732392d0a124fd40ca01082cf8f538ef
Farrelll <137156152+Farrelx9@users.noreply.github.com>
2024-05-22 23:50:34 +0700

    Update Login.jsx

--- src/assets/Auth/Login.jsx
+++ src/assets/Auth/Login.jsx
@@ -3,7 +3,6 @@ import React from "react";
 export default function Login() {
   return (
     <div className="bg-gradient-to-b from-green-200 to-white h-screen ">
-      <img src=""></img>
       <div>
         <div>Login</div>
       </div>
