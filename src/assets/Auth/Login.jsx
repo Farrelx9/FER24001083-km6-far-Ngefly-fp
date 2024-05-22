37c0c4631428cd97a82eda0c664bad83560f5d51
Farrelll <137156152+Farrelx9@users.noreply.github.com>
2024-05-22 23:48:21 +0700

    upd

--- src/assets/Auth/Login.jsx
+++ src/assets/Auth/Login.jsx
@@ -2,7 +2,8 @@ import React from "react";
 
 export default function Login() {
   return (
-    <div className="bg-gradient-to-r from-green-200 to-white h-screen flex items-center justify-center">
+    <div className="bg-gradient-to-b from-green-200 to-white h-screen ">
+      <img src="logo.png"></img>
       <div>
         <div>Login</div>
       </div>
