8a9a61d08ff2d673ad1c98ae013950e1f82fd242
Farrelll <137156152+Farrelx9@users.noreply.github.com>
2024-06-13 13:55:01 +0700

    upd

--- src/assets/Properties/Modal.jsx
+++ src/assets/Properties/Modal.jsx
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { Children } from "react";
+import { AiOutlineFullscreenExit } from "react-icons/ai";
 
-export default function Modal() {
+export default function Modal({ isVisible, onClose, children }) {
+  if (!isVisible) return null;
+  const handleClose = (e) => {
+    if (e.target.id === "wrapper") onClose();
+  };
   return (
-    <div>
-      <div></div>
+    <div
+      className="fixed inset-0 bg-black bg-opacity-25 flex justify-center items-center z-40 "
+      id="wrapper"
+      onClick={handleClose}
+    >
+      <div className="w-[702px] flex flex-col">
+        <button
+          className="text-black place-self-end mb-1"
+          onClick={() => onClose()}
+        >
+          <AiOutlineFullscreenExit size={20} />
+        </button>
+        <div className="bg-white p-2 rounded-lg">{children}</div>
+      </div>
     </div>
   );
 }
